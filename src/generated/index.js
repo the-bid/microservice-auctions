fa7dae22236767eff93885c4b5b782e097fec40a
Daniel Biales <bialesdaniel@gmail.com>
2019-02-15 15:58:51 -0500

    More package updating

--- src/generated/index.js
+++ src/generated/index.js
@@ -1,18 +1,18 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-const prisma_lib_1 = require('prisma-client-lib')
-const typeDefs = require('./prisma-schema').typeDefs
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
 
-const models = [
+var models = [
   {
-    name: 'Auction',
+    name: "Auction",
     embedded: false
   }
-]
+];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
   endpoint: `http://localhost:4466/the-bid-auctions/local`,
-  secret: `${process.env['PRISMA_SECRET']}`
-})
-exports.prisma = new exports.Prisma()
+  secret: `${process.env["PRISMA_SECRET"]}`
+});
+exports.prisma = new exports.Prisma();
